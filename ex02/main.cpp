1bf02989ef196b1166a199d35013bfda5a11f15d
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-03-28 13:11:07 -0300

    add

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,5 @@
-#include <iostream>
 
 int main(void)
 {
-    char    buff[512];
 
-    std::cout << "Hello world !" << std::endl;
-
-    std::cout << "Input a word: ";
-    std::cin >> buff;
-    std::cout << "You entered [" << buff << "]" << std::endl;
 }
