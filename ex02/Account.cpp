345bf2b1d7ad5baf9324cf0df14030c996ec8f56
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-04-01 13:21:46 -0300

    init ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,7 +1,63 @@
 #include "Account.hpp"
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello";
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts( void ) {
+	return _nbAccounts;
 }
+
+int	Account:: getTotalAmount( void ) {
+	return _totalAmount;
+}
+
+int	Account:: getNbDeposits( void ) {
+	return _totalNbDeposits;
+}
+
+int	Account:: getNbWithdrawals( void ) {
+	return _totalNbWithdrawals;
+}
+
+Account::Account( int initial_deposit )
+{
+	(void)initial_deposit;
+}
+
+Account::~Account()
+{
+
+}
+
+void	Account::_displayTimestamp(void)
+{
+
+}
+
+void	Account::displayAccountsInfos( void ) {
+
+}
+
+void	Account::makeDeposit( int deposit ) {
+	(void)deposit;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	(void)withdrawal;
+	return (false);
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (0);
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
+
