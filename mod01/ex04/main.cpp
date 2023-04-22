6cf29c2241771af3a47b5a802963a299c673c4d8
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-04-22 20:05:55 -0300

    added tests ex04

--- mod01/ex04/main.cpp
+++ mod01/ex04/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <fstream>
-#include <iterator>
 #include <filesystem>
-#include <sys/cdefs.h>
+
+void replace(std::ifstream &file, std::ofstream &output, std::string original, std::string toreplace)
+{
+	if (original.length() == 0)
+	{
+		output << file.rdbuf();
+		return;
+	}
+	std::string line;
+
+	while (!file.eof())
+	{
+		std::getline(file, line);
+		size_t pos = 0;
+		while ((pos = line.find(original, pos)) != std::string::npos)
+		{
+			line.erase(pos, original.size());
+			line.insert(pos, toreplace);
+			pos += toreplace.size();
+		}
+		output << line;
+
+		if (!file.eof())	//check if it is the last line of the file
+		{
+			output << std::endl;
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -10,45 +36,23 @@ int main(int argc, char **argv)
 	if (argc != 4)
 		return (std::cerr << "invalid number of arguments" << std::endl, 1);
 	
-	std::ifstream file (argv[1]);
-	std::ofstream output_file (std::string (std::string (argv[1]) + ".replace").c_str(), std::ios_base::trunc);
+	std::string filename = argv[1];
+	std::string original = argv[2];
+	std::string toreplace = argv[3];
 
-	//check file manipulation
+	//check file streams
+	std::ifstream file (filename.c_str());
 	if (!file.is_open())
 		return (std::cerr << "Unable to open file. Make sure the input file exists or have the right permissions" << std::endl, 1);
-	else if (!output_file.is_open())
+
+	std::ofstream output_file ((filename + ".replace").c_str(), std::ios_base::trunc);
+	if (!output_file.is_open())
 		return (std::cerr << "Unable to create file, try again" << std::endl, 1);
 
 	//check if file is empty
 	if (bool __attribute__((unused)) isEmpty = file.peek() == EOF)
 		return (std::cerr << "File is empty" << std::endl, 1);
 
-	std::string line;
-	std::string original = argv[2];
-	std::string toreplace = argv[3];
-	size_t		original_size;
-	bool		last_line = false;
-
-	original_size = original.size();
-
-	while (!file.eof())
-	{
-		std::getline(file, line);
-		size_t found = line.find(original);
-		if (found != std::string::npos)
-		{
-			line.erase(found, original_size);
-			line.insert(found, toreplace);
-		}
-		if (file.eof())
-		{
-			last_line = true;
-		}
-		output_file << line;
-		if (!last_line)
-		{
-			output_file << std::endl;	//append the endl only if last_line is false
-		}
-	}
+	replace(file, output_file, original, toreplace);
 	file.close();
 }
--- mod01/ex04/test.sh
+++ mod01/ex04/test.sh
@@ -1,3 +1,135 @@
 #!/bin/bash
 
-sed "
+BIN=sed_is_for_losers
+INFILE=tests.log
+EXPECTED=expected.log
+OUTFILE=$INFILE.replace
+ERROR_FILE=error.log
+
+echo "test 1"
+echo "one two three" > $INFILE
+echo "two two three" > $EXPECTED
+./$BIN $INFILE one two
+diff $EXPECTED $OUTFILE
+
+echo "test 2"
+echo "oone two three" > $INFILE
+echo "otwo two three" > $EXPECTED
+./$BIN $INFILE one two
+diff $EXPECTED $OUTFILE
+
+echo "test 3"
+echo "the recursion module caused automatic race condition" > $INFILE
+echo "the recursoin module caused automatic race conditoin" > $EXPECTED
+./$BIN $INFILE io oi
+diff $EXPECTED $OUTFILE
+
+echo "test 4"
+echo "invalid number of arguments" > $EXPECTED
+./$BIN 2> $OUTFILE
+diff $EXPECTED $OUTFILE
+
+echo "test 5"
+echo "invalid number of arguments" > $EXPECTED
+./$BIN $INFILE 2> $OUTFILE
+diff $EXPECTED $OUTFILE
+
+echo "test 6"
+echo "pull request from SCRUM caused polymorphism bug" > $INFILE
+echo "pullrequestfromSCRUMcausedpolymorphismbug" > $EXPECTED
+./$BIN $INFILE " " ""
+diff $EXPECTED $OUTFILE
+
+echo "test 7"
+echo "uffbuffbuffbuffbuffbuf" > $INFILE
+echo "ufferbufferbufferbufferbufferbuf" > $EXPECTED
+./$BIN $INFILE "ff" "ffer"
+diff $EXPECTED $OUTFILE
+
+echo "test 8"
+echo "aiAiAIaiAiaI" > $INFILE
+echo "AIAiAIAIAiaI" > $EXPECTED
+./$BIN $INFILE "ai" "AI"
+diff $EXPECTED $OUTFILE
+
+echo "test 9"
+echo "loop race" > $INFILE
+echo "loop race" > $EXPECTED
+./$BIN $INFILE "abc" "bca"
+diff $EXPECTED $OUTFILE
+
+echo "test 10"
+echo "Sed is for losers" > $INFILE
+echo "Replace is for winners" > $EXPECTED
+./$BIN $INFILE "Sed is for losers" "Replace is for winners"
+diff $EXPECTED $OUTFILE
+
+echo "test 11"
+echo "Sed is for losers" > $INFILE
+echo "Replace is for winners" > $EXPECTED
+./$BIN $INFILE "Sed is for losers" "Replace is for winners"
+diff $EXPECTED $OUTFILE
+
+echo "test 12"
+echo "none" > $INFILE
+echo "none" > $EXPECTED
+./$BIN $INFILE "none" "none"
+diff $EXPECTED $OUTFILE
+
+echo "test 13"
+echo "err" > $INFILE
+echo "Unable to open file. Make sure the input file exists or have the right permissions" > $EXPECTED
+./$BIN err$INFILE "1" "2" &>$OUTFILE
+diff $EXPECTED $OUTFILE
+
+echo "test 14"
+echo "err" > $INFILE
+echo "Unable to open file. Make sure the input file exists or have the right permissions" > $EXPECTED
+chmod 000 $INFILE
+./$BIN $INFILE "1" "2" &>$OUTFILE
+chmod 644 $INFILE
+diff $EXPECTED $OUTFILE
+
+echo "test 15"
+echo "clean" > $INFILE
+echo "clean" > $EXPECTED
+./$BIN $INFILE "cleaned" "err" &>$OUTFILE
+diff $EXPECTED $OUTFILE
+
+echo "test 16"
+echo "barra abc" > $INFILE
+echo "barra	abc" > $EXPECTED
+./$BIN $INFILE " " "	"
+diff $EXPECTED $OUTFILE
+
+echo "test 17"
+echo "barra abc" > $INFILE
+echo "barra\tabc" > $EXPECTED
+./$BIN $INFILE " " "\t"
+diff $EXPECTED $OUTFILE
+
+echo "test 18"
+echo "ccccccccccccccccccccccccccccccc" > $INFILE
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" > $EXPECTED
+./$BIN $INFILE "c" "x"
+diff $EXPECTED $OUTFILE
+
+echo "test 19"
+echo "ccccccccccccccccccccccccccccccc" > $INFILE
+echo "" > $EXPECTED
+./$BIN $INFILE "c" ""
+diff $EXPECTED $OUTFILE
+
+echo "test 20"
+echo "ccccccccccccccccccccccccccccccc" > $INFILE
+echo "ccccccccccccccccccccccccccccccc" > $EXPECTED
+./$BIN $INFILE "" "c"
+diff $EXPECTED $OUTFILE
+
+echo "test 21"
+> $INFILE
+echo "File is empty" > $EXPECTED
+./$BIN $INFILE "a" "a" 2> $ERROR_FILE
+diff $EXPECTED $ERROR_FILE
+
+#Credits: eandre-f
