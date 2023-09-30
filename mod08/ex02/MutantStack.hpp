a3b9cb203eebc330421f23fc81fdc1710f1da2a4
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-09-30 10:36:31 -0300

    finished

--- mod08/ex02/MutantStack.hpp
+++ mod08/ex02/MutantStack.hpp
@@ -3,7 +3,107 @@
 
 #include <iostream>
 #include <algorithm>
+#include <stack>
+#include <vector>
+#include <deque>
 #include <limits>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(void);
+		MutantStack(const MutantStack &copy);
+		MutantStack&operator=(const MutantStack &copy);
+		~MutantStack(void);
+
+		typedef typename std::stack<T>::container_type			_container;	//alias for each container iterator
+		typedef typename _container::iterator					iterator;
+		typedef typename _container::const_iterator				const_iterator;
+		typedef typename _container::reverse_iterator			reverse_iterator;
+		typedef typename _container::const_reverse_iterator		const_reverse_iterator;
+
+		iterator				begin();			//declaring the iterator functions
+		iterator				end();
+		const_iterator			begin() const;
+		const_iterator			end() const;
+		reverse_iterator		rbegin();
+		reverse_iterator		rend();
+		const_reverse_iterator	rbegin() const;
+		const_reverse_iterator	rend() const;
+
+	private:
+
+};
+
+template <typename T>
+MutantStack<T>::MutantStack(void) : std::stack<T>()
+{
+
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T> &copy) : std::stack<T>(copy)
+{
+
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &copy)
+{
+	std::stack<T>::operator=(copy);
+	return *this;
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack(void)
+{
+
+}
+
+template <typename T> typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return this->c.begin();	//"c" access the underlying container. In this case, deque
+}
+
+template <typename T> typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return this->c.end();
+}
+
+template <typename T> typename MutantStack<T>::const_iterator MutantStack<T>::begin() const
+{
+	return this->c.begin();
+}
+
+template <typename T> typename MutantStack<T>::const_iterator MutantStack<T>::end() const
+{
+	return this->c.end();
+}
+
+template <typename T> typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin()
+{
+	return this->c.rbegin();
+}
+
+template <typename T> typename MutantStack<T>::reverse_iterator MutantStack<T>::rend()
+{
+	return this->c.rend();
+}
+
+template <typename T> typename MutantStack<T>::const_reverse_iterator MutantStack<T>::rbegin() const
+{
+	return this->c.rbegin();
+}
+
+template <typename T> typename MutantStack<T>::const_reverse_iterator MutantStack<T>::rend() const
+{
+	return this->c.rend();
+}
+
+
+
+
 
 #endif //EX02_MUTANTSTACK_HPP
 
--- mod08/ex02/main.cpp
+++ mod08/ex02/main.cpp
@@ -1,1 +1,119 @@
 #include "MutantStack.hpp"
+
+int main(void)
+{
+	//The problem: stack is not iterable
+	{
+		std::stack<int> stackOverflow;
+
+		stackOverflow.push(std::numeric_limits<int>::max() - 3);
+		stackOverflow.push(std::numeric_limits<int>::max() - 2);
+		stackOverflow.push(std::numeric_limits<int>::max() - 1);
+
+		std::cout << "STACK ELEMENTS: " << std::endl;
+		while(!stackOverflow.empty())
+		{
+			int element = stackOverflow.top();
+			stackOverflow.pop();
+
+			std::cout << element << std::endl;
+		}
+	}
+
+	//The solution: let's make it iterable!
+	//There are two options one could think of, which is or implementing a
+	//custom logic to access and process its elements, or using its underlying
+	//containers, such as deque (double-ended queue)
+
+	//1) Implementing a custom logic
+
+	{
+		std::stack<int> stackOverflow;
+		std::vector<int> ahYeah;
+
+		stackOverflow.push(std::numeric_limits<int>::max() - 3);
+		stackOverflow.push(std::numeric_limits<int>::max() - 2);
+		stackOverflow.push(std::numeric_limits<int>::max() - 1);
+
+		//assigning the stack value
+		std::cout << "VECTOR (underlying container) ELEMENTS: " << std::endl;
+		while (!stackOverflow.empty())
+		{
+			ahYeah.push_back(stackOverflow.top());
+			stackOverflow.pop();
+		}
+
+		//printing from vector (ahYeah)
+		for (size_t i = 0; i < ahYeah.size(); ++i)
+		{
+			std::cout << ahYeah[i] << std::endl;
+		}
+	}
+	std::cout << std::endl;
+
+	//still, we are making a copy to another deck. There's certainly a better
+	//way to do this. Then, let's use...
+
+	//2)Underlying container
+
+	{
+		MutantStack<int> mutantStack;
+
+		mutantStack.push(1);
+		mutantStack.push(2);
+		mutantStack.push(3);
+	
+		std::cout << "Using begin() and end() functions:" << std::endl;
+		for (MutantStack<int>::iterator it = mutantStack.begin(); it != mutantStack.end(); ++it)
+		{
+			std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+
+		std::cout << "Using rbegin() and rend() functions:" << std::endl;
+		for (MutantStack<int>::reverse_iterator it = mutantStack.rbegin(); it != mutantStack.rend(); ++it) {
+			std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+
+		mutantStack.pop();
+		mutantStack.pop();
+		mutantStack.pop();
+		std::cout << std::endl;
+	}
+
+	{
+		//3) Subject Tests
+		MutantStack<int> mstack;
+
+		std::cout << "Subject Tests" << std::endl;
+		mstack.push(5);
+		mstack.push(17);
+
+		std::cout << "What\'s on top: " << mstack.top() << std::endl;
+
+		mstack.pop();
+
+		std::cout << "Size: " << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+
+		//if you uncomment this, you'll see the iterator changing
+		//++it;
+		while (it != ite)
+		{
+			std::cout << *it << ", ";
+			++it;
+		}
+		std::cout << std::endl;
+	}
+}
